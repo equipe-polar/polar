7be2d48b5790fe3e28995a3cee5a7c945f162884
guilhermecauan7k-debug <guilhermecauan7k@gmail.com>
2026-08-13 10:31:16 -0300

    Remove basePath from ESLint configuration
    remoção de linha de codigo

--- apps/api/eslint.config.js
+++ apps/api/eslint.config.js
@@ -5,7 +5,6 @@ export default tseslint.config(
   js.configs.recommended,
   ...tseslint.configs.recommended,
   {
-    basePath: "../..",
     files: ["**/*.ts"],
     languageOptions: {
       ecmaVersion: 2022,
